86b73bb70ca55078b1317c2241d63fb2c7408a25
Christopher Chow <christopher.chow@kinesis.org>
2017-01-13 14:06:39 +1100

    fix(moment): Don't use ES6 in moment extensions

--- vendor/moment-ext.js
+++ vendor/moment-ext.js
@@ -1,15 +1,15 @@
 moment.fn.fiscalYearFirstDay = function() {
-  let year = this.month() < 6 ? this.year() - 1 : this.year();
-  let month = 6;
+  var year = this.month() < 6 ? this.year() - 1 : this.year();
+  var month = 6;
 
-  let newDate = new Date(year, month);
+  var newDate = new Date(year, month);
   return moment(newDate);
 };
 
 moment.fn.fiscalYearLastDay = function() {
-  let year = this.month() > 5 ? this.year() + 1 : this.year();
-  let month = 5;
+  var year = this.month() > 5 ? this.year() + 1 : this.year();
+  var month = 5;
 
-  let newDate = new Date(year, month, 30);
+  var newDate = new Date(year, month, 30);
   return moment(newDate).endOf('day');
 };
