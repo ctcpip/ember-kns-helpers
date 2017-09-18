4ded6c06a675d8bc232b7e524a6e0e5d6a751dc4
Steve Bell <stephen.bell@kinesis.org>
2017-09-18 17:26:07 +1000

    feat(gocd): Add GoCD configuration

--- testem.js
+++ testem.js
@@ -3,9 +3,7 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS",
     "Chrome",
-    "Firefox"
   ],
   "launch_in_dev": [
   ]
