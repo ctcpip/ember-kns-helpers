eea3382be84cc727fd9c38234186441f16b73865
David Nettle <david.nettle@kinesis.org>
2018-08-14 15:16:31 +1000

    feat(format-number): Fix for when number comes in as an array, and allow zeroFormat argument (default 0).

--- addon/helpers/format-number.js
+++ addon/helpers/format-number.js
@@ -1,10 +1,15 @@
 import numeral from 'numeral';
+import Ember from 'ember';
 import Helper from 'ember-helper';
 
-export function formatNumber([number, format = 'kns']) {
+export function formatNumber([number, format = 'kns', zeroFormat = '0']) {
+  if (Ember.isArray(number)) { number = number[0]; }
+
   if (typeof number === 'undefined') {
     return '-';
   }
+
+  numeral.zeroFormat(zeroFormat);
 
   return numeral(number).format(format);
 }
